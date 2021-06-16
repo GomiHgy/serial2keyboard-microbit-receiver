de6a48bf13c40d9c4b1f1794c660604bb45d94ad
GomiHgy <h.furukawa1@gmail.com>
2021-06-17 00:08:09 +0900

    シリアル通信の設定をyin:bitに合わせた

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ bufr.setUint8(0, receivedNumber);
 serial.writeBuffer(bufr)
 })
 radio.onReceivedString(function (receivedString) {
-    led.toggle(4, 4)
+    led.toggle(4, 0)
     serial.writeString(receivedString)
 })
 radio.setTransmitPower(7)
@@ -13,7 +13,7 @@ radio.setFrequencyBand(7)
 radio.setGroup(7)
 radio.sendString("")
 serial.redirect(
-SerialPin.P0,
+SerialPin.P2,
 SerialPin.P1,
 BaudRate.BaudRate115200
 )
